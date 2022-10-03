d3f9e2a7f0183af1118f5a74c902c106e4b7db05
Tarun <tarunchawla.7463@gmail.com>
2022-10-03 18:25:30 -0400

    Changed photo and added proper links to the projects and did little bit addition in javascript, so that nav closes on click on links

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ if (mq.matches){
     const btn = document.getElementById("button");
     const mnav= document.getElementsByClassName("header__nav")[0];
     const menu_icon = document.querySelector(".menu-icon");
+    mnav.addEventListener("click", function(){
+        if(mnav.style.display === "block"){
+            mnav.style.display = 'none';
+            menu_icon.style.backgroundColor = "white";
+            menu_icon.style.transform = "rotate(0deg)";
+            document.documentElement.style.setProperty('--before', "translate(-20px,-12px)");
+            document.documentElement.style.setProperty('--after', "translate(-20px,12px)");
+        }
+    });
     btn.addEventListener("click", function(){
         if(mnav.style.display != "block"){
             mnav.style.display = "block";
